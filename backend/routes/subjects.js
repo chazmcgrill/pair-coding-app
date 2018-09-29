6e3e764575bf175b1e2dfd99c55d431c3e7d3f10
“Richard Middleton” <Richard@richardmiddleton.me>
2018-09-29 14:41:46 +0200

    Serve and backend setup

---
+++ backend/routes/subjects.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Subject = require('../models/subject');
+
+// Get request
+router.get('/', (req, res) => {
+    Subject.find()
+        .sort({ date: -1 })
+        .then(subject => res.json(subject))
+});
+
+// CREATE - Add new subject to database
+router.post('/', (req, res) => {
+	const newSubject = new Subject({
+		name: req.body.name,
+		date: req.body.date
+    });
+    newSubject.save().then(subject => res.json(subject));
+});
+
+// Delete request
+router.delete('/:id', (req, res) => {
+    Subject.findOneAndRemove({ id: req.params.id },  function(err) {
+        if (err)
+            res.send(err);
+        else
+            res.json({ message: 'Subject Deleted!'});
+    });
+});
+module.exports = router;
