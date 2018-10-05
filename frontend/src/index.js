694d33313ecf3a2968fa56da086e75bc1434f60e
“Richard Middleton” <Richard@richardmiddleton.me>
2018-10-05 13:25:43 +0200

    react router setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes } from './routes'; // where we are going to specify our routes
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './assets/css/main.css';
+
+
+ReactDOM.render(
+  <Router>
+    <Routes />
+  </Router>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
--- frontend/src/views/Curriculum/Curriculum.js
+++ frontend/src/views/Curriculum/Curriculum.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import './assets/css/main.css';
 
-class App extends Component {
+
+class Curriculum extends Component {
 
   state = {
     certificates: []
@@ -47,4 +47,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Curriculum;
