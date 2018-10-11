a076128802c6685bb111fac65d35800174628b80
“Richard Middleton” <Richard@richardmiddleton.me>
2018-10-11 19:08:35 +0200

    made accordion stateless

--- frontend/src/app/components/ListView.js
+++ frontend/src/app/components/ListView.js
@@ -1,16 +1,11 @@
-import React, { Component } from "react";
-
-import {
-  Accordion,
-  AccordionItem,
-  AccordionItemTitle,
-  AccordionItemBody
-} from "react-accessible-accordion";
-
-class ListView extends Component {
+import React from "react";
+import { Accordion } from "react-accessible-accordion";
+import Certificate from './Certificate';
 
 
-  selected(e) {
+
+
+function selected(e) {
     const element = document.getElementById(e.target.id);
 
     if(element && element.classList.contains('accordion__title')) {
@@ -18,41 +13,13 @@ class ListView extends Component {
     }
   }
 
-  render() {
-    const { certificates } = this.props;
-    return (
-
-        <Accordion onClick={(e) => this.selected(e)} accordion={false}>
+  const ListView = ({ certificates }) => (
+        <Accordion onClick={(e) => selected(e)} accordion={false}>
             {// Display all the names of the Certificates
-            certificates.map((cert, i) => (
-            <AccordionItem sections={cert.sections} key={i}>
-                <AccordionItemTitle>{cert.title}</AccordionItemTitle>
-                <AccordionItemBody>
-                {// Display all the names of the sections
-                cert.sections.map((section, i) => (
-                    <AccordionItem key={i}>
-                    <AccordionItemTitle>{section.name}</AccordionItemTitle>
-                    <AccordionItemBody>
-                    <div className="accordion__body--container">
-                        <p>This section includes the challenges: </p>
-                        <ul>
-                        {// List out all the challenges
-                        section.list.map((item, i) => (
-                            <li key={i}>{item}</li>
-                        ))}
-                        </ul>
-                        <p>Want to pair-program with someone in this section? click the button to see who's studying.</p>
-                        <button className="btn">Find A Partner</button>
-                        </div>
-                    </AccordionItemBody>
-                    </AccordionItem>
-                ))}
-                </AccordionItemBody>
-            </AccordionItem>
+                certificates.map((cert, i) => (
+                    <Certificate cert={cert} />
             ))}
         </Accordion>
     );
-  }
-}
 
 export default ListView;
---
+++ frontend/src/app/components/Section.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { AccordionItem, AccordionItemTitle, AccordionItemBody } from "react-accessible-accordion";
+
+const Section = ({ section }) => (
+
+    <AccordionItem key={section.name}>
+        <AccordionItemTitle>{section.name}</AccordionItemTitle>
+        <AccordionItemBody>
+            <div className="accordion__body--container">
+                <p>This section includes the challenges: </p>
+                <ul>
+                    {// List out all the challenges
+                        section.list.map((item, i) => (
+                            <li key={i}>{item}</li>
+                        ))}
+                </ul>
+                <p>Want to pair-program with someone in this section? click the button to see who's studying.</p>
+                <button className="btn">Find A Partner</button>
+            </div>
+        </AccordionItemBody>
+    </AccordionItem>
+
+);
+
+export default Section;
