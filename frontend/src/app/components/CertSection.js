60b7d040205b989382f8e3cba829a3b8839e7e1e
“Richard Middleton” <Richard@richardmiddleton.me>
2018-10-13 20:12:02 +0200

    triangle bracket for sections

--- frontend/src/app/components/CertSection.js
+++ frontend/src/app/components/CertSection.js
@@ -3,7 +3,7 @@ import CertChallenge from './CertChallenge';
 
 const CertSection = ({ section, handleSectionClick }) => (
     <div className="accordion-section" key={section.name}>
-        <p className="accordion-cert__title" onClick={handleSectionClick}>{section.name}</p>
+        <p className="accordion-cert__title" onClick={handleSectionClick}>&gt; &nbsp; {section.name}</p>
         <CertChallenge section={section}/>
     </div>
 )
