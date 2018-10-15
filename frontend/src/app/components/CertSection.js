ce1e053bffb6f41458fda5ed35999868b098eaae
“Richard Middleton” <Richard@richardmiddleton.me>
2018-10-15 20:46:41 +0200

    color to open selection

--- frontend/src/app/components/CertSection.js
+++ frontend/src/app/components/CertSection.js
@@ -1,9 +1,13 @@
 import React from "react";
 import CertChallenge from './CertChallenge';
 
+function className(section) {
+    return section.open ? 'accordion-cert__title colored' : 'accordion-cert__title'
+}
+
 const CertSection = ({ section, handleSectionClick }) => (
     <div className="accordion-section" key={section.name}>
-        <p className="accordion-cert__title" onClick={handleSectionClick}>&gt; &nbsp; {section.name}</p>
+        <p className={className(section)}  onClick={handleSectionClick}>&gt; &nbsp; {section.name}</p>
         <CertChallenge section={section}/>
     </div>
 )
