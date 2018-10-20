a6a3570dd1f1d7227cf7ab9f1c09a8a6475e1414
chazmcgrill <charlietaylordev@gmail.com>
2018-10-20 13:36:31 +0100

    Adds LoaderSpinner Component & Styling

--- frontend/src/app/pages/Curriculum.js
+++ frontend/src/app/pages/Curriculum.js
@@ -1,32 +1,35 @@
 import React from "react";
 import CertAccordion from '../components/CertAccordion';
+import LoadingSpinner from "../components/LoadingSpinner";
 
-const Curriculum = ({ loaded, handleCertClick, certificates, callAPI, handleSectionClick }) => (
-      <section className="App">
-        <div className="row">
-          <h1>Curriculum</h1>
-        </div>
+const Curriculum = ({ loaded, handleCertClick, certificates, callAPI, handleSectionClick }) => {
+  if (!loaded) callAPI()
+  return (
+    <section className="App">
+      <div className="row">
+        <h1>Curriculum</h1>
+      </div>
 
-        <div className="row">
-          <div className="col col--main">
-            {loaded ? <CertAccordion certificates={certificates} handleCertClick={handleCertClick} handleSectionClick={handleSectionClick} /> : <h2>Loading...</h2> && 
-            callAPI()}
-            
-          </div>
-          <div className="col col--side">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-              Donec elementum ligula eu sapien
-            </p>
-          </div>
+      <div className="row">
+        <div className="col col--main">
+          {loaded ? <CertAccordion certificates={certificates} handleCertClick={handleCertClick} handleSectionClick={handleSectionClick} /> : <LoadingSpinner />}
+          
         </div>
-      </section>
-    );
+        <div className="col col--side">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+            Donec elementum ligula eu sapien
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
 
 export default Curriculum;
