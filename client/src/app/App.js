454eef48b2d65ca1ed8532f0179e6260d504a451
chazmcgrill <charlietaylordev@gmail.com>
2018-11-03 11:08:28 +0000

    Adds Sockets Setup

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
+import io from 'socket.io-client';
 import Curriculum from './pages/Curriculum';
 import NavBar from './components/NavBar';
 import Modal from './components/Modal';
@@ -8,12 +9,67 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Grid from './pages/Grid';
 
+const socket = io('http://localhost:5000');
+
 export default class App extends Component {
   state = {
     certificates: [],
     loaded: false,
-    isModalOpen: false
+    isModalOpen: false,
+    user: {},
   };
+
+  popup = null;
+
+  // Routinely checks the popup to re-enable the login button 
+  // if the user closes the popup without authenticating.
+  checkPopup() {
+    const check = setInterval(() => {
+      const { popup } = this
+      if (!popup || popup.closed || popup.closed === undefined) {
+        clearInterval(check)
+        this.setState({ disabled: '' })
+      }
+    }, 1000)
+  }
+
+  // Launches the popup on the server and passes along the socket id so it 
+  // can be used to send back user data to the appropriate socket on 
+  // the connected client.
+  openPopup() {
+    const width = 600, height = 600
+    const left = (window.innerWidth / 2) - (width / 2)
+    const top = (window.innerHeight / 2) - (height / 2)
+    const url = `${API_URL}/twitter?socketId=${socket.id}`
+
+    return window.open(url, '',
+      `toolbar=no, location=no, directories=no, status=no, menubar=no, 
+      scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
+      height=${height}, top=${top}, left=${left}`
+    )
+  }
+
+  // Kicks off the processes of opening the popup on the server and listening 
+  // to the popup. It also disables the login button so the user can not 
+  // attempt to login to the provider twice.
+  startAuth() {
+    if (!this.state.disabled) {
+      this.popup = this.openPopup()
+      this.checkPopup()
+      this.setState({ disabled: 'disabled' })
+    }
+  }
+
+  closeCard() {
+    this.setState({ user: {} })
+  }
+
+  componentDidMount() {
+    socket.on('user', user => {
+      this.popup.close();
+      this.setState({user});
+    })
+  }
 
   callAPI = () => {
     fetch("api/subjects")
