8211e8deadc07cd615f5c07981e86be704716d45
chazmcgrill <charlietaylorcoder@gmail.com>
2018-11-11 10:45:57 +0000

    Adds Auth Backend

--- routes/auth.js
+++ routes/auth.js
@@ -1,60 +1,31 @@
 const express = require('express');
-const http = require('http');
 const router = express.Router();
-// const passport = require('passport');
-const session = require('express-session');
-const cors = require('cors');
-const socketio = require('socket.io');
-const { Strategy: GithubStrategy } = require('passport-github');
+const passport = require('passport');
 
-const GITHUB_CONFIG = {
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackUrl: 'localhost:5000/api/auth/callback',
-}
+// Setting up the passport middleware for each of the OAuth providers
+const githubAuth = passport.authenticate('github')
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-
-app.use(express.json());
-app.use(passport.initialize);
-
-
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
-
-app.use(session({
-  secret: 'testing123',
-  resave: true,
-  saveUninitialized: true,
-}))
-
-passport.serializeUser((user, cb) => cb(null, user));
-passport.deserializeUser((obj, cb) => cb(null, obj));
-
-passport.use(new GithubStrategy(
-  GITHUB_CONFIG, 
-  (accessToken, refreshToken, profile, cb) => {
-    const user = {
-      name: profile.username,
-      photo: profile.photos[0].value.replace(/_normal/, '')
-    }
-    cb(null, user);
-  })
-);
-
-const githubAuth = passport.autheniticate('github');
-
-const addSocketIdtoSession = (req, res, next) => {
-  req.session.socketId = req.query.socketId;
-  next();
-}
-
-router.get('/', addSocketIdtoSession, githubAuth)
-
+// Routes that are triggered by the callbacks from each OAuth provider once 
+// the user has authenticated successfully
 router.get('/callback', githubAuth, (req, res) => {
-  io.in(req.session.socketId).emit('user', req.user);
-  res.end();
-});
+  const io = req.app.get('io')
+  const user = {
+    name: req.user.username,
+    photo: req.user.photos[0].value
+  }
+  io.in(req.session.socketId).emit('github', user)
+  res.end()
+})
+
+// This custom middleware allows us to attach the socket id to the session
+// With that socket id we can send back the right user info to the right 
+// socket
+router.use((req, res, next) => {
+  req.session.socketId = req.query.socketId
+  next()
+})
+
+// Routes that are triggered on the client
+router.get('/', githubAuth)
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
 const path = require('path');
+const http = require('http');
 const passport = require('passport');
+const session = require('express-session');
+const cors = require('cors');
+const socketio = require('socket.io');
+// const passportInit = require('./auth/passport-init');
+const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const app = express();
 
 require('dotenv').config();
-require('./services/passport');
 require('./models/user');
 
 const subjects = require('./routes/subjects');
 const authRoutes = require('./routes/auth');
 
-// Setup for CORS
-app.use(cors());
+const { Strategy: GithubStrategy } = require('passport-github');
+
+const GITHUB_CONFIG = {
+	clientID: process.env.GITHUB_CLIENT_ID,
+	clientSecret: process.env.GITHUB_CLIENT_SECRET,
+	callbackUrl: 'localhost:5000/api/auth/callback',
+}
+
+// Allowing passport to serialize and deserialize users into sessions
+passport.serializeUser((user, cb) => cb(null, user))
+passport.deserializeUser((obj, cb) => cb(null, obj))
+
+// Adding each OAuth provider's strategy to passport
+passport.use(new GithubStrategy(GITHUB_CONFIG, (accessToken, refreshToken, profile, cb) => cb(null, profile)))
+
+let server = http.createServer(app);
 
 // Body Parsing
 app.use(express.json());
@@ -23,7 +40,33 @@ app.use(express.urlencoded({ extended: false }));
 // Authentication
 app.use(cookieParser());
 app.use(passport.initialize());
-app.use(passport.session());
+
+// Setup for CORS / Accept requests from our client
+app.use(cors({
+	origin: process.env.CLIENT_ORIGIN,
+}));
+
+// saveUninitialized: true allows us to attach the socket id to the session
+// before we have athenticated the user
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: true,
+	saveUninitialized: true
+}));
+
+// Connect to Mongo
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log(err));
+
+// Connecting sockets to the server and adding them to the request 
+// so that we can access them later in the controller
+const io = socketio(server)
+app.set('io', io);
+
+// Catch a start up request so that a sleepy Heroku instance can  
+// be responsive as soon as possible
+app.get('/wake-up', (req, res) => res.send('👍'))
 
 // Use Routes
 app.use('/api/subjects', subjects);
@@ -38,11 +81,7 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
-// Connect to Mongo
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-	.then(() => console.log('MongoDB Connected'))
-	.catch(err => console.log(err));
-
 // Server
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+server.listen(process.env.PORT || 5000, () => {
+	console.log(console.log(`Server started on port ${5000}`));
+})
