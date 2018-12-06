c2008852f542224c2328f460bb455630cc9c251d
chazmcgrill <charlietaylorcoder@gmail.com>
2018-12-06 18:53:28 +0000

    Tidies Server Main File

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const http = require('http');
 const passport = require('passport');
 const session = require('express-session');
@@ -11,6 +10,43 @@ const app = express();
 const User = require('./models/user');
 require('dotenv').config();
 
+const subjects = require('./routes/subjects');
+const authRoutes = require('./routes/auth');
+
+// connect to database
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log(err));
+
+// app setup
+app.use(express.json({type: '*/*'}));
+app.use(cookieParser());
+app.use(passport.initialize());
+
+// Setup for CORS / Accept requests from our client
+app.use(cors({ origin: process.env.CLIENT_ORIGIN }));
+
+// saveUninitialized: true allows us to attach the socket id to the session
+// before we have athenticated the user
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: true,
+	saveUninitialized: true
+}));
+
+// Connecting sockets to the server and adding them to the request 
+// so that we can access them later in the controller
+let server = http.createServer(app);
+const io = socketio(server)
+app.set('socketio', io);
+
+// server setup
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+	console.log(`Server started on port ${port}`);
+})
+
+// MOVE OUT TO OWN FILE
 const { Strategy: GithubStrategy } = require('passport-github');
 
 const GITHUB_CONFIG = {
@@ -25,81 +61,29 @@ passport.deserializeUser((obj, cb) => cb(null, obj))
 
 // Adding each OAuth provider's strategy to passport
 passport.use(new GithubStrategy(GITHUB_CONFIG, (accessToken, refreshToken, profile, cb) => {
-    
+
 	// save the user to the database
-    const user = { 
+	const user = {
 		githubId: profile.id,
 		displayName: profile.username,
 		avatar: profile.photos[0].value
-    }
+	}
 
-    User.findOne({githubId: user.githubId})
-      .then(res => {
-        if(res) {
-            console.log('user is already signed up');
-        }
-        else {
-          new User(user)
-            .save()
-            .catch(err => console.log(err));
-        }
-  });
+	User.findOne({ githubId: user.githubId })
+		.then(res => {
+			if (res) {
+				console.log('user is already signed up');
+			}
+			else {
+				new User(user)
+					.save()
+					.catch(err => console.log(err));
+			}
+		});
 
-    cb(null, profile)
-  })
-)
-
-let server = http.createServer(app);
-
-// Body Parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Authentication
-app.use(cookieParser());
-app.use(passport.initialize());
-
-// Setup for CORS / Accept requests from our client
-app.use(cors({
-	origin: process.env.CLIENT_ORIGIN,
-}));
-
-// saveUninitialized: true allows us to attach the socket id to the session
-// before we have athenticated the user
-app.use(session({
-	secret: process.env.SESSION_SECRET,
-	resave: true,
-	saveUninitialized: true
-}));
-
-const subjects = require('./routes/subjects');
-const authRoutes = require('./routes/auth');
-
-// Connect to Mongo
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-	.then(() => console.log('MongoDB Connected'))
-	.catch(err => console.log(err));
-
-// Connecting sockets to the server and adding them to the request 
-// so that we can access them later in the controller
-const io = socketio(server)
-app.set('socketio', io);
+	cb(null, profile)
+}))
 
 // Use Routes
 app.use('/api/subjects', subjects);
 app.use('/api/auth', authRoutes);
-
-// Serve static files from the React app
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client', 'build')));
-	
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-	});
-}
-
-// Server
-const port = process.env.PORT || 5000;
-server.listen(port, () => {
-	console.log(`Server started on port ${port}`);
-})
