26c0227c9ae724ebd9405c09309ed0a1095892c4
chazmcgrill <charlietaylorcoder@gmail.com>
2018-12-06 19:35:29 +0000

    Refactors Passport Strategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,37 +3,31 @@ const passport = require('passport');
 const { Strategy: GithubStrategy } = require('passport-github');
 const User = require('../models/user');
 
-const GITHUB_CONFIG = {
-    clientID: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackUrl: 'http://127.0.0.1:5000/api/auth/callback',
-}
-
-// // Allowing passport to serialize and deserialize users into sessions
+// allows passport to serialize and deserialize users into sessions
 passport.serializeUser((user, cb) => cb(null, user))
 passport.deserializeUser((obj, cb) => cb(null, obj))
 
-// Adding each OAuth provider's strategy to passport
-passport.use(new GithubStrategy(GITHUB_CONFIG, (accessToken, refreshToken, profile, cb) => {
-
-    // save the user to the database
-    const user = {
-        githubId: profile.id,
-        displayName: profile.username,
-        avatar: profile.photos[0].value
-    }
-
-    User.findOne({ githubId: user.githubId })
-        .then(res => {
-            if (res) {
-                console.log('user is already signed up');
-            }
-            else {
-                new User(user)
-                    .save()
-                    .catch(err => console.log(err));
-            }
+// 
+const githubLogin = new GithubStrategy({
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackUrl: 'http://127.0.0.1:5000/api/auth/callback',
+}, (accessToken, refreshToken, profile, done) => {
+    
+    User.findOne({ githubId: profile.id }, (err, user) => {
+        if (err) return done(err);
+        if (user) return done(null, profile);
+        const newUser = new User({
+            githubId: profile.id,
+            displayName: profile.username,
+            avatar: profile.photos[0].value
         });
+        newUser.save(err => {
+            if (err) return done(err);
+            return done(null, profile);
+        })
+    });
+});
 
-    cb(null, profile)
-}));
+// use the strategy
+passport.use(githubLogin);
