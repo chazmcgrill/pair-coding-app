a6484b9a7c562ac899ec59a2b16fa7357fc5d89e
chazmcgrill <charlietaylorcoder@gmail.com>
2018-12-08 16:19:11 +0000

    Refactors Router / Curriculum

--- client/src/app/pages/Curriculum.js
+++ client/src/app/pages/Curriculum.js
@@ -1,30 +1,95 @@
-import React from "react";
+import React, {Component} from "react";
 import CertAccordion from '../components/CertAccordion';
 import LoadingSpinner from "../components/LoadingSpinner";
 
-const Curriculum = ({ loaded, handleCertClick, certificates, callAPI, handleSectionClick }) => {
-  if (!loaded) callAPI()
-  return (
-    <main>
-      <div className="row">
-        <div className="col col--main">
-          {loaded ? <CertAccordion certificates={certificates} handleCertClick={handleCertClick} handleSectionClick={handleSectionClick} /> : <LoadingSpinner />}
+class Curriculum extends Component {
+    state = {
+      certificates: [],
+      loaded: false,
+    }
+
+    callAPI = () => {
+    fetch("api/subjects")
+      .then(res => res.json())
+      .then(certificates => {
+        this.setState({
+          certificates,
+          loaded: true
+        });
+      })
+      .catch(err => {
+        console.log(err)
+      });
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  handleCertClick = (id) => {
+    const certificates = this.state.certificates.map(cert => (
+      { ...cert, open: !cert.open && cert._id === id }
+    ));
+    this.setState({certificates})
+  }
+
+  handleSectionClick = (id) => {
+    const certificates = this.state.certificates.map(cert => {
+      const sections = cert.sections.map(section => ({ ...section, open: !section.open && section._id === id}))
+      return {...cert, sections}
+    })
+    this.setState({certificates})
+  }
+
+  render() {
+    const { certificates } = this.state;
+    return (
+      <main>
+        <div className="row">
+          <div className="col col--main">
+            {this.state.loaded ? <CertAccordion certificates={certificates} handleCertClick={this.handleCertClick} handleSectionClick={this.handleSectionClick} /> : <LoadingSpinner />}
+          </div>
+          <div className="col col--side">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+              Donec elementum ligula eu sapien
+            </p>
+          </div>
         </div>
-        <div className="col col--side">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Donec elementum ligula eu sapien
-          </p>
-        </div>
-      </div>
-    </main>
-  )
+      </main>
+    )
+  }
 }
+
+// const Curriculum = ({ loaded, handleCertClick, certificates, callAPI, handleSectionClick }) => {
+//   if (!loaded) callAPI();
+//   return (
+//     <main>
+//       <div className="row">
+//         <div className="col col--main">
+//           {loaded ? <CertAccordion certificates={certificates} handleCertClick={handleCertClick} handleSectionClick={handleSectionClick} /> : <LoadingSpinner />}
+//         </div>
+//         <div className="col col--side">
+//           <p>
+//             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+//             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+//             enim ad minim veniam, quis nostrud exercitation ullamco laboris
+//             nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+//             reprehenderit in voluptate velit esse cillum dolore eu fugiat
+//             nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+//             sunt in culpa qui officia deserunt mollit anim id est laborum.
+//             Donec elementum ligula eu sapien
+//           </p>
+//         </div>
+//       </div>
+//     </main>
+//   )
+// }
 
 export default Curriculum;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/App';
-import './index.sass';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import App from './app/App';
+import Grid from './app/pages/Grid';
+import Landing from './app/pages/Landing';
+import Curriculum from './app/pages/Curriculum';
+import './index.sass';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />, document.getElementById('root'),
-);
+	<BrowserRouter>
+    	<App>
+			<Route path="/" exact component={Landing} />
+			<Route path="/grid" exact component={Grid} />
+			<Route path="/curriculum" exact component={Curriculum} />
+		</App>
+	</BrowserRouter>
+, document.getElementById('root'));
 
 registerServiceWorker();
