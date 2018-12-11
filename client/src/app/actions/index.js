9c6a99e36851e14354ded36128bc93b8d96b71aa
chazmcgrill <charlietaylorcoder@gmail.com>
2018-12-11 19:14:09 +0000

    Removes Callbacks Causing Errors

--- client/src/app/actions/index.js
+++ client/src/app/actions/index.js
@@ -2,22 +2,20 @@ import axios from 'axios';
 import { GET_CERTS, GET_CERTS_ERROR, OPEN_CERT, OPEN_SECTION, ADD_USER, TOGGLE_LOGIN_MODAL, GET_CONVERSATIONS, GET_CONVERSATIONS_ERROR } from './types';
 
 // Curriculum Actions
-export const getCurriculum = (callback) => async dispatch => {
+export const getCurriculum = () => async dispatch => {
     try {
         const response = await axios.get('http://localhost:5000/api/subjects');
         dispatch({ type: GET_CERTS, payload: response.data });
-        callback();
     } catch (e) {
         dispatch({ type: GET_CERTS_ERROR, payload: 'Error Fetching Data' })
     }
 };
 
 // Messages Actions
-export const getConversations = (callback) => async dispatch => {
+export const getConversations = () => async dispatch => {
     try {
         const response = await axios.get('http://localhost:5000/api/conversations');
         dispatch({ type: GET_CONVERSATIONS, payload: response.data });
-        callback();
     } catch (e) {
         dispatch({ type: GET_CONVERSATIONS_ERROR, payload: 'Error Fetching Data' })
     }
