cb72173d6e8f682ad11d60f4e9f63ff29856dd1e
chazmcgrill <charlietaylorcoder@gmail.com>
2018-12-21 17:06:12 +0000

    Makes Reviews & Features Responsive / Improves Hero Button & Reviews Data

--- client/src/app/components/landingpage/Hero.js
+++ client/src/app/components/landingpage/Hero.js
@@ -9,7 +9,7 @@ export default () => (
             <div className="hero-text">
                 <h1>Find people to pair code with right now</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, exercitationem. Modi, dolores tempora. Numquam officia debitis consequuntur tempore aut? Minima debitis qui vel excepturi, provident perferendis quibusdam expedita tempora quo?</p>
-                <div className="btn btn--lg">Learn More</div>
+                <a href="#features" className="btn btn--lg hero-text_btn">Learn More</a>
             </div>
         </div>
     </div>
--- client/src/app/components/landingpage/Reviews.js
+++ client/src/app/components/landingpage/Reviews.js
@@ -14,9 +14,12 @@ export default () => (
                     <img className="review_avatar_img" src={Avatar1} alt="avatar 1" />
                 </div>
                 <div className="review_text">
-                    <p>Similique maiores, totam dicta nobis quaerat, optio soluta? Voluptatibus, distinctio!</p>
+                    <p className="review_quote">&ldquo;Similique maiores, totam dicta nobis quaerat, optio soluta? Voluptatibus, distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. At animi natus nemo ab obcaecati aperiam. Natus!&rdquo;</p>
                     <h4>Jane Something</h4>
-                    <p>CEO pf Something</p>
+                    <p>
+                        CEO of
+                        <strong> Something</strong>
+                    </p>
                 </div>
             </div>
             <div className="review">
@@ -24,9 +27,12 @@ export default () => (
                     <img className="review_avatar_img" src={Avatar2} alt="avatar 1" />
                 </div>
                 <div className="review_text">
-                    <p>Similique maiores, totam dicta nobis quaerat, optio soluta? Voluptatibus, distinctio!</p>
-                    <h4>Jane Something</h4>
-                    <p>CEO pf Something</p>
+                    <p className="review_quote">&ldquo;Similique maiores, totam dicta nobis quaerat, optio soluta? Voluptatibus, distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. At animi natus nemo ab obcaecati aperiam. Natus!&rdquo;</p>
+                    <h4>John Smith</h4>
+                    <p>
+                        CEO of
+                        <strong> Something</strong>
+                    </p>
                 </div>
             </div>
             <div className="review">
@@ -34,9 +40,12 @@ export default () => (
                     <img className="review_avatar_img" src={Avatar3} alt="avatar 1" />
                 </div>
                 <div className="review_text">
-                    <p>Similique maiores, totam dicta nobis quaerat, optio soluta? Voluptatibus, distinctio!</p>
-                    <h4>Jane Something</h4>
-                    <p>CEO pf Something</p>
+                    <p className="review_quote">&ldquo;Similique maiores, totam dicta nobis quaerat, optio soluta? Voluptatibus, distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. At animi natus nemo ab obcaecati aperiam. Natus!&rdquo;</p>
+                    <h4>Darren Brown</h4>
+                    <p>
+                        CEO of
+                        <strong> Something</strong>
+                    </p>
                 </div>
             </div>
             <div className="review">
@@ -44,9 +53,12 @@ export default () => (
                     <img className="review_avatar_img" src={Avatar4} alt="avatar 1" />
                 </div>
                 <div className="review_text">
-                    <p>Similique maiores, totam dicta nobis quaerat, optio soluta? Voluptatibus, distinctio!</p>
-                    <h4>Jane Something</h4>
-                    <p>CEO pf Something</p>
+                    <p className="review_quote">&ldquo;Similique maiores, totam dicta nobis quaerat, optio soluta? Voluptatibus, distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. At animi natus nemo ab obcaecati aperiam. Natus!&rdquo;</p>
+                    <h4>Sarah Smith</h4>
+                    <p>
+                        CEO of
+                        <strong> Something</strong>
+                    </p>
                 </div>
             </div>
         </div>
